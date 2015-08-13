4d8ac5067593ceb9b3880ecb7ee074935f7d75fe
terma <artem.stasuk@gmail.com>
2015-08-13 00:15:26 -0400

    Reduce count of $watch

--- server/src/main/webapp/js/engine.js
+++ server/src/main/webapp/js/engine.js
@@ -913,3 +913,23 @@ Array.prototype.mkString = function (delimiter) {
     }
     return result;
 };
+
+function countWatchers() {
+    var root = angular.element(document).injector().get('$rootScope');
+    var count = root.$$watchers ? root.$$watchers.length : 0; // include the current scope
+    var pendingChildHeads = [root.$$childHead];
+    var currentScope;
+
+    while (pendingChildHeads.length)
+    {
+        currentScope = pendingChildHeads.shift();
+
+        while (currentScope)
+        {
+            count += currentScope.$$watchers ? currentScope.$$watchers.length : 0;
+            pendingChildHeads.push(currentScope.$$childHead);
+            currentScope = currentScope.$$nextSibling;
+        }
+    }
+    console.log('watchers on page: ' + count);
+};
