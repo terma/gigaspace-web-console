641ffecdcc74587a4da8976412612e4c663902a0
terma <artem.stasuk@gmail.com>
2015-11-18 19:45:24 -0500

    Avoid window namespace polution by fixed header js staff

--- server/src/main/webapp/js/fixed-header.js
+++ server/src/main/webapp/js/fixed-header.js
@@ -1,57 +1,60 @@
-var fixedHeaders = {
+(function () {
+    var fixedHeaders = {
 
-    removeFixed: function (table) {
-        var fixedHeader = table.parent().children(".fixed");
-        fixedHeader.remove();
-    },
+        removeFixed: function (table) {
+            var fixedHeader = table.parent().children(".fixed");
+            fixedHeader.remove();
+        },
 
-    createFixed: function (table) {
-        var fixedHeader = $(table[0].cloneNode(false));
+        createFixed: function (table) {
+            var fixedHeader = $(table[0].cloneNode(false));
 
-        fixedHeader.append(table.children("thead").clone());
+            fixedHeader.append(table.children("thead").clone());
 
-        fixedHeader.addClass("fixed").removeClass("fixMe").insertBefore(table);
-        fixedHeader.css("width", table.width());
+            fixedHeader.addClass("fixed").removeClass("fixMe").insertBefore(table);
+            fixedHeader.css("width", table.width());
 
-        fixedHeader.find("th").each(function (index) {
-            var width = table.find("th").eq(index).width();
-            $(this).css("width", (width + 1));
-        });
+            fixedHeader.find("th").each(function (index) {
+                var width = table.find("th").eq(index).width();
+                $(this).css("width", (width + 1));
+            });
 
-        fixedHeader.show();
+            fixedHeader.show();
 
-        return fixedHeader;
-    },
+            return fixedHeader;
+        },
 
-    showFixed: function (table, offsetLeft, tableOffset) {
-        var fixedHeader = table.parent().children(".fixed");
+        showFixed: function (table, offsetLeft, tableOffset) {
+            var fixedHeader = table.parent().children(".fixed");
 
-        if (fixedHeader.length == 0) {
-            fixedHeader = fixedHeaders.createFixed(table);
+            if (fixedHeader.length == 0) {
+                fixedHeader = fixedHeaders.createFixed(table);
+            }
+
+            fixedHeader.css("left", tableOffset.left - offsetLeft);
+        },
+
+        onScroll: function () {
+            var offsetTop = $(document).scrollTop();
+            var offsetLeft = $(document).scrollLeft();
+
+            $(".fixMe").each(function () {
+                var table = $(this);
+                var tableOffset = table.offset();
+
+                var tableOffsetTop = tableOffset.top;
+                var tableOffsetBottom = tableOffsetTop + table.height() - table.children("thead").height();
+
+                if (offsetTop < tableOffsetTop || offsetTop > tableOffsetBottom) {
+                    fixedHeaders.removeFixed(table);
+                } else if (offsetTop >= tableOffsetTop && offsetTop <= tableOffsetBottom) {
+                    fixedHeaders.showFixed(table, offsetLeft, tableOffset);
+                }
+            });
         }
 
-        fixedHeader.css("left", tableOffset.left - offsetLeft);
-    },
+    };
 
-    onScroll: function () {
-        var offsetTop = $(document).scrollTop();
-        var offsetLeft = $(document).scrollLeft();
+    $(window).on("scroll", fixedHeaders.onScroll);
 
-        $(".fixMe").each(function () {
-            var table = $(this);
-            var tableOffset = table.offset();
-
-            var tableOffsetTop = tableOffset.top;
-            var tableOffsetBottom = tableOffsetTop + table.height() - table.children("thead").height();
-
-            if (offsetTop < tableOffsetTop || offsetTop > tableOffsetBottom) {
-                fixedHeaders.removeFixed(table);
-            } else if (offsetTop >= tableOffsetTop && offsetTop <= tableOffsetBottom) {
-                fixedHeaders.showFixed(table, offsetLeft, tableOffset);
-            }
-        });
-    }
-
-};
-
-$(window).on("scroll", fixedHeaders.onScroll);
+})();
