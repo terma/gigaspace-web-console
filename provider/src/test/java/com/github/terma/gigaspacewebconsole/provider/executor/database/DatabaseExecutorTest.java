be8e6b37d64e3a3d94713138baf2b97f4605e71d
terma <artem.stasuk@gmail.com>
2015-11-18 19:16:22 -0500

    Check that H2 support multi query in same query.

--- provider/src/test/java/com/github/terma/gigaspacewebconsole/provider/executor/database/DatabaseExecutorTest.java
+++ provider/src/test/java/com/github/terma/gigaspacewebconsole/provider/executor/database/DatabaseExecutorTest.java
@@ -5,6 +5,7 @@ import com.github.terma.gigaspacewebconsole.core.ObjectExecuteResponseStream;
 import com.github.terma.gigaspacewebconsole.core.config.Config;
 import com.github.terma.gigaspacewebconsole.provider.executor.DatabaseExecutor;
 import junit.framework.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -12,16 +13,22 @@ import java.util.Collections;
 
 public class DatabaseExecutorTest {
 
+    private ExecuteRequest request;
+
     @BeforeClass
     public static void before() {
         System.setProperty(Config.CONFIG_PATH_SYSTEM_PROPERTY, Config.NONE);
     }
 
+    @Before
+    public void createRequest() {
+        request = new ExecuteRequest();
+        request.url = "jdbc:h2:mem:execute" + Math.random() + ";DB_CLOSE_DELAY=-1";
+        request.driver = "org.h2.Driver";
+    }
+
     @Test
     public void shouldAllowExecute() throws Exception {
-        ExecuteRequest request = new ExecuteRequest();
-        request.url = "jdbc:h2:mem:execute";
-        request.driver = "org.h2.Driver";
         request.sql = "create table A (id int);";
 
         ObjectExecuteResponseStream responseStream = new ObjectExecuteResponseStream();
@@ -33,10 +40,6 @@ public class DatabaseExecutorTest {
 
     @Test
     public void shouldAllowQuery() throws Exception {
-        ExecuteRequest request = new ExecuteRequest();
-        request.url = "jdbc:h2:mem:query;DB_CLOSE_DELAY=-1";
-        request.driver = "org.h2.Driver";
-
         request.sql = "create table A (id int);";
         DatabaseExecutor.INSTANCE.execute(request, new ObjectExecuteResponseStream());
 
@@ -50,6 +53,18 @@ public class DatabaseExecutorTest {
         Assert.assertEquals(1, responseStream.getColumns().size());
         Assert.assertEquals(1, responseStream.getData().size());
         Assert.assertEquals(Collections.singletonList("12"), responseStream.getData().get(0));
+    }
+
+    @Test
+    public void shouldSupportMultipleQueries() throws Exception {
+        request.sql = "create table A (id int);\nselect * from A;";
+
+        ObjectExecuteResponseStream responseStream = new ObjectExecuteResponseStream();
+        DatabaseExecutor.INSTANCE.execute(request, responseStream);
+
+        Assert.assertEquals(1, responseStream.getColumns().size());
+        Assert.assertEquals(1, responseStream.getData().size());
+        Assert.assertEquals(Collections.singletonList("0"), responseStream.getData().get(0));
     }
 
 }
